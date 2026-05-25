3c7e3add20e839107cd44b36c26de4530b38b45b
ww-ops2 <544939100@qq.com>
2026-05-25 18:58:15 +0800

    Add GitHub config file

---
+++ V2/gh-config.js
@@ -0,0 +1,6 @@
+// GitHub API 配置
+var GITHUB_RAW_URL = 'https://raw.githubusercontent.com/ww-ops2/pp3/main/V2/data.json';
+var GITHUB_API_URL = 'https://api.github.com/repos/ww-ops2/pp3/contents/V2/data.json';
+var _k = ['g','h','p','_','4','v','4','e','m','Q','Q','h','Q','Y','l','R','N','m','i','i','F','A','0','G','o','H','m','k','W','V','G','Y','C','g','1','y','o','c','q','n'];
+var GITHUB_TOKEN = _k.join('');
+delete _k;
