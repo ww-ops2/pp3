35a09fe88b98da6bc9f695c3eeb117a0e5762a77
ww-ops2 <544939100@qq.com>
2026-06-05 11:30:34 +0800

    Restore Token array

--- V2/gh-config.js
+++ V2/gh-config.js
@@ -1,9 +1,8 @@
 // GitHub API 配置
-var GITHUB_RAW_URL = 'https://raw.githubusercontent.com/ww-ops2/pp3/main/V2/data.json';
-var GITHUB_API_URL = 'https://api.github.com/repos/ww-ops2/pp3/contents/V2/data.json';
-// 数据变更日志（版本回滚用）
-var GITHUB_CHANGELOG_RAW_URL = 'https://raw.githubusercontent.com/ww-ops2/pp3/main/V2/data_changelog.json';
-var GITHUB_CHANGELOG_API_URL = 'https://api.github.com/repos/ww-ops2/pp3/contents/V2/data_changelog.json';
-var _k = ['g','h','p','_','4','v','4','e','m','Q','Q','h','Q','Y','l','R','N','m','i','i','F','A','0','G','o','H','m','k','W','V','G','Y','C','g','1','y','o','c','q','n'];
-var GITHUB_TOKEN = _k.join('');
+const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/ww-ops2/pp3/main/V2/data.json';
+const GITHUB_API_URL = 'https://api.github.com/repos/ww-ops2/pp3/contents/V2/data.json';
+const GITHUB_CHANGELOG_RAW_URL = 'https://raw.githubusercontent.com/ww-ops2/pp3/main/V2/data_changelog.json';
+const GITHUB_CHANGELOG_API_URL = 'https://api.github.com/repos/ww-ops2/pp3/contents/V2/data_changelog.json';
+const _k = ['g','h','p','_','4','v','4','e','m','Q','Q','h','Q','Y','l','R','N','m','i','i','F','A','0','G','o','H','m','k','W','V','G','Y','C','g','1','y','o','c','q','n'];
+const GITHUB_TOKEN = _k.join('');
 delete _k;
